193311d0f0d53fe68ef8af2ba2344c2cfe46b6b1
lizeyliz <alizaeaton@gmail.com>
2024-11-22 09:22:41 -0800

    started add method, nothing much coded yet

--- AlizaMethods.java
+++ AlizaMethods.java
@@ -1,3 +1,44 @@
 public class AlizaMethods {
+    DatabaseNode root; //top of tree
+
+    //constructor: will change later
+    public AlizaMethods() {
+        this.root = null;
+    }//end constructor
+
+    //from textbook
+    /*public void add (int value) {
+        DatabaseNode overallRoot = add(overallRoot, value);
+    }
+    public DatabaseNode add(DatabaseNode root, int value){
+        if(root == null) {
+            root = new DatabaseNode(value);
+        } else if (root.idNum > value) {
+            root.left = add(root.left, value)
+        } else if(root.idNum < value) {
+            root.right = add(root.right, value);
+        }
+        return root;
+    }*/
     
+    //left side smaller, right side bigger
+    //slide 34 chapter 17 binary search trees
+    //start with recursion then change to iteration
+    /*public void add(DatabaseNode node) {
+        if(root == null) {
+            root = node;
+        } else if (node < current) { 
+            //start at top
+            DatabaseNode current = root;
+            //if left is empty, add node 
+        
+        } else if (node > current) {
+            //start at top
+            DatabaseNode current = root;
+
+        } else {
+            //invalid if # is same as one in tree
+        }
+
+    }//end add*/
 }
