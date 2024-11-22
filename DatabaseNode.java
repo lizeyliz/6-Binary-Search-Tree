9f8e81b83f8d55526d573020e66dba1579de35c5
lizeyliz <alizaeaton@gmail.com>
2024-11-22 08:25:27 -0800

    made object class

--- DatabaseNode.java
+++ DatabaseNode.java
@@ -1,3 +1,21 @@
+//object class
 public class DatabaseNode {
+    //instance variables
+    private int idNum;
+    //pointing to left and right
+    DatabaseNode left;
+    DatabaseNode right;
     
-}
+    //constructor
+    public DatabaseNode(int idNum){
+        this.idNum = idNum;
+    }//end constructor
+
+    public int getID(){
+        return idNum;
+    }//end getID
+
+    public void setID(int newID) {
+        this.idNum = newID;
+    }//endsetID
+}//end class DatabaseNode
