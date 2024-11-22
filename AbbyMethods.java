e02927df98b460e7448bdea97b534e960f71c2ff
Abby Jahns <2abbylouwho@gmail.com>
2024-11-22 09:40:50 -0800

    started preOrder method in AbbyMethods

--- AbbyMethods.java
+++ AbbyMethods.java
@@ -1,3 +1,23 @@
 public class AbbyMethods {
+    DatabaseNode root; //top of tree
     
+    //start at root, print all left nodes then backtrack and print right
+    public void preOrder() {
+        if (root == null) {
+            System.out.println("Empty Database");
+        } else {
+            DatabaseNode current = root;
+            DatabaseNode parent = null;
+            System.out.print("Pre-order: " + root.getID());
+            //print all left nodes
+            while (current.left != null) {
+                System.out.print( " " + current.getID());
+                parent = current;
+                current = current.left;
+            }//end while loop
+            while (parent.right != null) {
+
+            }//end while loop
+        }//end if/else statement
+    }//end preOrder method
 }
