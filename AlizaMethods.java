a7320d2dd610f6dc646253aa38e5afae1ce11071
lizeyliz <alizaeaton@gmail.com>
2024-11-23 07:51:41 -0800

    working on add method, need to test

--- AlizaMethods.java
+++ AlizaMethods.java
@@ -1,4 +1,5 @@
 //https://liveexample.pearsoncmg.com/dsanimation13ejava/BSTeBook.html
+//Chat GPT
 public class AlizaMethods {
     DatabaseNode root; //top of tree
 
@@ -7,66 +8,40 @@ public class AlizaMethods {
         this.root = null;
     }//end constructor
 
-    //use variable called key?
+    //add a node to the binary search tree
     public void add(DatabaseNode newNode) {
+        //if tree is empty
+        if(root == null) {
+            root = newNode;
+            return; //end method here if root == null
+        }//end if statement
+
+        //starting from the top
         DatabaseNode current = root;
-        boolean placed = false;
-        while(placed == false){
-            if(root == null) {
-                root = newNode;
-                placed = true;
-            } else if (newNode.getID() < root.getID()) {
+        //while loop for placement if tree is not empty
+        while(true){ //loops until break is called
+            if (newNode.getID() < current.getID()) {
                 if(current.left == null) {
-                    current.left = newNode;
-                    placed = true;
+                    current.left = newNode; //insert to the left
+                    break;
                 } else {
-                    //move to next node to the left
-                    current = current.left;
-                }
-            } else if (newNode.getID() > root.getID()) {
+                    current = current.left; //move to next node to the left
+                }//end inner if/else 1
+            } else if (newNode.getID() > current.getID()) {
                 if(current.right  == null) {
-                    current.right = newNode;
-                    placed = true;
+                    current.right = newNode; //insert to the right
+                    break;
                 } else {
-                    current = current.right;
-                }
+                    current = current.right; //move to next node to the right
+                }//end inner if/else 2
+            } else { //if newNode is equal to current
+                //add exception handling here
+                System.out.println("Node is a duplicate and cannot be placed.");
+                break;
             }//end outer if/else
         }//end while loop
-    }
+    }//end add method
+}//end class AlizaMethods
 
-    //from textbook
-    /*public void add (int value) {
-        DatabaseNode overallRoot = add(overallRoot, value);
-    }
-    public DatabaseNode add(DatabaseNode root, int value){
-        if(root == null) {
-            root = new DatabaseNode(value);
-        } else if (root.idNum > value) {
-            root.left = add(root.left, value)
-        } else if(root.idNum < value) {
-            root.right = add(root.right, value);
-        }
-        return root;
-    }*/
-    
-    //left side smaller, right side bigger
-    //slide 34 chapter 17 binary search trees
-    //start with recursion then change to iteration
-    /*public void add(DatabaseNode node) {
-        if(root == null) {
-            root = node;
-        } else if (node < current) { 
-            //start at top
-            DatabaseNode current = root;
-            //if left is empty, add node 
-        
-        } else if (node > current) {
-            //start at top
-            DatabaseNode current = root;
-
-        } else {
-            //invalid if # is same as one in tree
-        }
-
-    }//end add*/
-}
+//To do:
+//-Add exception handling for duplicates
