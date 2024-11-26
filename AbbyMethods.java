62babdda8f87506b3156449a2c2fbb459156f5f4
Abby Jahns <2abbylouwho@gmail.com>
2024-11-25 21:44:40 -0800

    added iteration postOrder in AbbyMethods

--- AbbyMethods.java
+++ AbbyMethods.java
@@ -1,23 +1,37 @@
+import java.util.Stack;
+
 public class AbbyMethods {
     DatabaseNode root; //top of tree
     
     //start at root, print all left nodes then backtrack and print right
-    public void preOrder() {
+    public void postOrder() {
         if (root == null) {
             System.out.println("Empty Database");
         } else {
+            Stack<DatabaseNode> stack = new Stack<>();
             DatabaseNode current = root;
-            DatabaseNode parent = null;
-            System.out.print("Pre-order: " + root.getID());
-            //print all left nodes
-            while (current.left != null) {
-                System.out.print( " " + current.getID());
-                parent = current;
-                current = current.left;
-            }//end while loop
-            while (parent.right != null) {
+            boolean check = true;
 
-            }//end while loop
-        }//end if/else statement
+            while (true) { //infinite loop until break
+                //go to extreme left
+                while (current != null && check) {
+                    stack.push(current);
+                    current = current.left;
+                }
+                if (stack.empty()) {
+                    break;
+                }
+                //to avoid infinite loop
+                if (current != stack.peek().right) {
+                    current = stack.peek().right;
+                    check = true;
+                    continue;
+                }
+                //if not caught in any above special case
+                current = stack.pop();
+                System.out.print(current.getID() + " ");
+                check = false;
+            }//end outer while loop
+        }//end it/else statement
     }//end preOrder method
-}
+}//end class
